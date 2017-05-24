6bbd806ea14173f7190a1866030de47987aee18e
Ulf Lilleengen <lulf@redhat.com>
2017-05-24 09:46:02 +0200

    Print container log

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ then
 fi
 
 docker pull ppatierno/qdrouterd:0.8.0-repo || fail=1
-docker run -d --name qdrouterd -p 5672:5672 -p 55673:55673 -v $PWD/src/test/resources:/conf ppatierno/qdrouterd:0.8.0-repo qdrouterd --conf /conf/qdrouterd.conf || fail=1
-
+container=`docker run -d --name qdrouterd -p 5672:5672 -p 55673:55673 -v $PWD/src/test/resources:/conf ppatierno/qdrouterd:0.8.0-repo qdrouterd --conf /conf/qdrouterd.conf || fail=1`
+echo "CONTAINER: $container"
 sleep 10
 echo "Docker images:" 
 docker ps -a
+echo "Logs: "
+docker logs $container
 
 
 trap "docker stop qdrouterd; docker rm qdrouterd" EXIT
